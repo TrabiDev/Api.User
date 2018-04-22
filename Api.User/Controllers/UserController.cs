3744fb74afffce6d9e1eadc83de8c20615c1803a
Everton Miranda <notreve.miranda@gmail.com>
2018-04-22 13:59:07 -0300

    add api.user

---
+++ Api.User/Controllers/UserController.cs
@@ -0,0 +1,32 @@
+﻿using System.Threading.Tasks;
+using Api.User.Domain.Interfaces.Controller;
+using Api.User.Domain.Interfaces.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Api.User.Controllers
+{
+    [Produces("application/json")]
+    [Route("api/User")]
+    public class UserController : Controller, IUserController
+    {
+
+        private readonly IUserService _service;
+
+        public UserController(IUserService service)
+        {
+            _service = service;
+        }
+
+        [HttpGet]
+        [Route("GetUsersByKindOfService")]
+        public async Task<IActionResult> GetUsersByKindOfService(string request)
+        {
+
+            var result = await _service.GetUsersByKindOfService(request);
+
+            return Ok(result);
+
+        }
+
+    }
+}
